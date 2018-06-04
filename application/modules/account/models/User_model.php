359fb080618f9d7ff0b2dc4ebcb1a22feb3a8128
HanhDeveloper <hanh.cho.do@gmail.com>
2018-06-04 17:52:44 +0700

    add

--- application/modules/account/models/User_model.php
+++ application/modules/account/models/User_model.php
@@ -3,6 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class User_model extends MY_Model
 {
+    protected $table = 'users';
+
     public function update_account($data)
     {
         $this->update(get_user_id(), $data);
--- application/modules/login/models/User_model.php
+++ application/modules/login/models/User_model.php
@@ -3,4 +3,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class User_model extends MY_Model
 {
+    protected $table = 'users';
 }
